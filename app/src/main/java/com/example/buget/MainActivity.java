67458feb5e0c45db935e4ba633f51baea1fce72c
Dae <dae.park@utexas.edu>
2019-01-27 23:21:14 -0800

    Butterknife @OnClick causes crash - reproduced

--- app/src/main/java/com/example/buget/MainActivity.java
+++ app/src/main/java/com/example/buget/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.buget;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private ProfileFragment profileFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_profile);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new ProfileFragment(), "profile").commit();
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/buget/ProfileFragment.java
@@ -0,0 +1,167 @@
+package com.example.buget;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.HashMap;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class ProfileFragment extends Fragment {
+    @BindView(R.id.fragment_profile_add_bank_account_webview)
+    WebView plaidLinkWebview;
+
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        unbinder = ButterKnife.bind(this, view);
+//        view.findViewById(R.id.fragment_profile_add_bank_account_button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onClickLinkBankAccount();
+//            }
+//        });
+        return view;
+    }
+
+    @OnClick(R.id.fragment_profile_add_bank_account_button)
+    public void onClickLinkBankAccount() {
+        initializedPlaidLink();
+    }
+
+    private void initializedPlaidLink() {
+        // Initialize Link
+        HashMap<String, String> linkInitializeOptions = new HashMap<String, String>();
+        linkInitializeOptions.put("product", "auth");
+        linkInitializeOptions.put("apiVersion", "v2"); // set this to "v1" if using the legacy Plaid API
+        linkInitializeOptions.put("env", "sandbox");
+        linkInitializeOptions.put("clientName", "Indie");
+        linkInitializeOptions.put("selectAccount", "true");
+        linkInitializeOptions.put("webhook", "http://requestb.in");
+        linkInitializeOptions.put("baseUrl", "https://cdn.plaid.com/link/v2/stable/link.html");
+        // If initializing Link in PATCH / update mode, also provide the public_token
+        // linkInitializeOptions.put("token", "PUBLIC_TOKEN")
+
+        // Generate the Link initialization URL based off of the configuration options.
+        final Uri linkInitializationUrl = generateLinkInitializationUrl(linkInitializeOptions);
+
+        // Modify Webview settings - all of these settings may not be applicable
+        // or necesscary for your integration.
+        WebSettings webSettings = plaidLinkWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        // Initialize Link by loading the Link initiaization URL in the Webview
+        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+
+        // Override the Webview's handler for redirects
+        // Link communicates success and failure (analogous to the web's onSuccess and onExit
+        // callbacks) via redirects.
+        plaidLinkWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Parse the URL to determine if it's a special Plaid Link redirect or a request
+                // for a standard URL (typically a forgotten password or account not setup link).
+                // Handle Plaid Link redirects and open traditional pages directly in the  user's
+                // preferred browser.
+                Uri parsedUri = Uri.parse(url);
+                if (parsedUri.getScheme().equals("plaidlink")) {
+                    String action = parsedUri.getHost();
+                    HashMap<String, String> linkData = parseLinkUriData(parsedUri);
+
+                    if (action.equals("connected")) {
+                        // User successfully linked
+                        Log.d("Public token: ", linkData.get("public_token"));
+                        Log.d("Account ID: ", linkData.get("account_id"));
+                        Log.d("Account name: ", linkData.get("account_name"));
+                        Log.d("Institution type: ", linkData.get("institution_type"));
+                        Log.d("Institution name: ", linkData.get("institution_name"));
+
+                        // Reload Link in the Webview
+                        // You will likely want to transition the view at this point.
+                        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+                    } else if (action.equals("exit")) {
+                        // User exited
+                        // linkData may contain information about the user's status in the Link flow,
+                        // the institution selected, information about any error encountered,
+                        // and relevant API request IDs.
+                        Log.d("User status in flow: ", linkData.get("status"));
+                        // The requet ID keys may or may not exist depending on when the user exited
+                        // the Link flow.
+                        Log.d("Link request ID: ", linkData.get("link_request_id")!=null? linkData.get("link_request_id") : "");
+                        Log.d("API request ID: ", linkData.get("plaid_api_request_id"));
+
+                        // Reload Link in the Webview
+                        // You will likely want to transition the view at this point.
+                        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+                    } else if (action.equals("event")) {
+                        // The event action is fired as the user moves through the Link flow
+                        Log.d("Event name: ", linkData.get("event_name"));
+                    } else {
+                        Log.d("Link action detected: ", action);
+                    }
+                    // Override URL loading
+                    return true;
+                } else if (parsedUri.getScheme().equals("https") ||
+                        parsedUri.getScheme().equals("http")) {
+                    // Open in browser - this is most  typically for 'account locked' or
+                    // 'forgotten password' redirects
+                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    // Override URL loading
+                    return true;
+                } else {
+                    // Unknown case - do not override URL loading
+                    return false;
+                }
+            }
+        });
+    }
+
+    // Generate a Link initialization URL based on a set of configuration options
+    public Uri generateLinkInitializationUrl(HashMap<String, String> linkOptions) {
+        Uri.Builder builder = Uri.parse(linkOptions.get("baseUrl"))
+                .buildUpon()
+                .appendQueryParameter("isWebview", "true")
+                .appendQueryParameter("isMobile", "true");
+        for (String key : linkOptions.keySet()) {
+            if (!key.equals("baseUrl")) {
+                builder.appendQueryParameter(key, linkOptions.get(key));
+            }
+        }
+        return builder.build();
+    }
+
+    // Parse a Link redirect URL querystring into a HashMap for easy manipulation and access
+    public HashMap<String, String> parseLinkUriData(Uri linkUri) {
+        HashMap<String, String> linkData = new HashMap<String, String>();
+        for (String key : linkUri.getQueryParameterNames()) {
+            linkData.put(key, linkUri.getQueryParameter(key));
+        }
+        return linkData;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
